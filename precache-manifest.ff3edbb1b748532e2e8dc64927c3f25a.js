3945a9278fc5ba92254098db57ad61a1064ddcf4
jonathanclare <jonathan.david.clare@gmail.com>
2018-11-22 14:58:34 +0000

    Added build files

---
+++ precache-manifest.ff3edbb1b748532e2e8dc64927c3f25a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ce6228d6d7190fc87552",
+    "url": "/static/js/main.ce6228d6.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "ce6228d6d7190fc87552",
+    "url": "/static/css/main.fcc58cb2.chunk.css"
+  },
+  {
+    "revision": "ed93b7b8e224f740efded48a75be8714",
+    "url": "/index.html"
+  }
+];
