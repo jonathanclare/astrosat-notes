a645163ea368a339ce74c3d6da5148e33efdfcc4
jonathanclare <jonathan.david.clare@gmail.com>
2018-11-22 15:16:08 +0000

    Updated build

---
+++ precache-manifest.4d5c8ccd14fcf0ec56b6a065fb8bfd27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/astrosat-notes/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2371c74b5f5ee1015560",
+    "url": "/astrosat-notes/static/js/runtime~main.2371c74b.js"
+  },
+  {
+    "revision": "1bb055f09ac2c2500601",
+    "url": "/astrosat-notes/static/js/main.1bb055f0.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/astrosat-notes/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "1bb055f09ac2c2500601",
+    "url": "/astrosat-notes/static/css/main.fcc58cb2.chunk.css"
+  },
+  {
+    "revision": "74e086b1fedeb38ba7a292f9fce11d12",
+    "url": "/astrosat-notes/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ff3edbb1b748532e2e8dc64927c3f25a.js"
+  "/astrosat-notes/precache-manifest.4d5c8ccd14fcf0ec56b6a065fb8bfd27.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/astrosat-notes/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
