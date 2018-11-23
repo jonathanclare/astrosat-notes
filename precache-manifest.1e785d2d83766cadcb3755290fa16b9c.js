f5d3c07beef00cf048b43c34e46f248d19bf1d1c
jonathanclare <jonathan.david.clare@gmail.com>
2018-11-23 13:01:43 +0000

    build

---
+++ precache-manifest.1e785d2d83766cadcb3755290fa16b9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2371c74b5f5ee1015560",
+    "url": "/astrosat-notes/static/js/runtime~main.2371c74b.js"
+  },
+  {
+    "revision": "d1d62d7ec79d808c37f1",
+    "url": "/astrosat-notes/static/js/main.d1d62d7e.chunk.js"
+  },
+  {
+    "revision": "07183b3538132f8bae70",
+    "url": "/astrosat-notes/static/js/1.07183b35.chunk.js"
+  },
+  {
+    "revision": "d1d62d7ec79d808c37f1",
+    "url": "/astrosat-notes/static/css/main.32eb72ab.chunk.css"
+  },
+  {
+    "revision": "1e2710e64ff32373d02fdfa5a1116c28",
+    "url": "/astrosat-notes/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/astrosat-notes/precache-manifest.588a5c899601b652399991fbbc8ec530.js"
+  "/astrosat-notes/precache-manifest.1e785d2d83766cadcb3755290fa16b9c.js"
 );
 
 workbox.clientsClaim();
